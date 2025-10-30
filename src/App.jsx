fbbd92c8f9951d18f65fbdacb79d5db80583da6c
SaisagarYT <saisagarsylada@gmail.com>
2025-10-30 20:39:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+import WebImages from './assets/WebImages'
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex bg-(--dark-green)'>
+      <section className='w-1/2 bg-white px-30 items-start justify-center gap-5 flex flex-col'>
+        <div className='text-3xl flex gap-2 items-center font-medium'>
+          <i className="ri-space-ship-fill rotate-45"></i>
+          <h1>App Name</h1>
+        </div>
+        <h1 className='text-8xl font-bold'>Download Our App</h1>
+        <p className='text-lg text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo sed voluptates enim veniam, sit libero odio sunt, nulla doloremque dolores vitae ut ullam ex eius delectus minus corporis totam neque!lkj aljfald aklaaljfoee sl loole oekd allakdkla</p>
+        <ul className='w-110 flex items-center'>
+          <button>
+            <img src={WebImages.playstore} className='' alt="" />
+          </button>
+          <button>
+            <img src={WebImages.appstore} className=' ' alt="" />
+          </button>
+        </ul>
+      </section>
+      <section className='w-1/2 flex items-center justify-center'>
+        <div className='w-80 h-155 bg-(--gold)'>
+
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/WebImages.jsx
@@ -0,0 +1,6 @@
+const WebImages = {
+    playstore:"src/assets/images/playstore.png",
+    appstore:"src/assets/images/appstore.png",
+}
+
+export default WebImages;
